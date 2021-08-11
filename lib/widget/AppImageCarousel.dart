e107fb5b1396c59532ba3cc77f4587d16f1e5be7
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-08-11 14:14:31 +0530

    Image Carousel Completed

--- lib/widget/AppImageCarousel.dart
+++ lib/widget/AppImageCarousel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:wine_hq/responsive/responsive.dart';
 
 class AppImageCarousel extends StatelessWidget {
   const AppImageCarousel({
@@ -9,8 +10,10 @@ class AppImageCarousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(50),
+      borderRadius: BorderRadius.circular(10),
       child: ImageSlideshow(
+        isLoop: true,
+        autoPlayInterval: 2500,
         children: [
           Image.asset(
             'wine_examples_assets/eveonline.jpg',
@@ -24,9 +27,47 @@ class AppImageCarousel extends StatelessWidget {
             'wine_examples_assets/goggalaxy.jpg',
             fit: BoxFit.fill,
           ),
+          Image.asset(
+            'wine_examples_assets/matlab.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/metatrader5.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/outlook2010.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/photoshopcc2015.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/reason.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/sims3.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/skyrim_specialed.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/steam.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset(
+            'wine_examples_assets/wizard101.jpg',
+            fit: BoxFit.fill,
+          ),
         ],
-        width: MediaQuery.of(context).size.width * 0.8,
-        height: 250,
+        width: isDesktop(context)
+            ? MediaQuery.of(context).size.width * 0.7
+            : MediaQuery.of(context).size.width * 0.95,
+        height: MediaQuery.of(context).size.height * 0.7,
       ),
     );
   }
